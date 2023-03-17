4c43da9746a6bc65543cb1429e746bc081c5bacd
90degs2infty <90degs2infty@posteo.org>
2023-03-17 14:11:15 +0100

    Adapt sources to resemble a new platform

--- src/gen_greet.cpp
+++ src/gen_greet.cpp
@@ -1,6 +1,6 @@
 #include <common/gen_greet.h>
 
 std::string generate_greeting() {
-    std::string greet = "Hello, this is a greeting from platform_a!";
+    std::string greet = "Hello, this is a greeting from platform_b!";
     return greet;
 }
